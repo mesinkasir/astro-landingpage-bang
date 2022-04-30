bcd27d5b895e7a830de6e36bd272c767efaa2a29
mesinkasir <hockeycorpmarketing@gmail.com>
2022-04-30 23:18:50 +0700

    Komat kamit 5 files modified

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # astro-landingpage
 
 Live codding on StackBlitz ⚡️
+
+Test demo [https://bangresto.vercel.app](https://bangresto.vercel.app/)
--- src/pages/data/home.json.ts
+++ src/pages/data/home.json.ts
@@ -13,59 +13,72 @@ export async function get() {
       intro1: 'Waiters ',
       cover1:
         'https://1.bp.blogspot.com/-amRq5gD-YUI/YO6PoeOtPvI/AAAAAAAAPw0/x87qw7Oq1sUyYdRhafrL55MZr8WglodrACLcBGAsYHQ/s1342/pos%2Bkasir.png',
-      conintro1: ' ',
+      conintro1:
+        'Include dengan waiters order yang memudahkan akses via tablet maupun via smartphone dalam bekerja, waiters ini nantinya akan mencatat pesanan pelanggan include dengan detail meja yang ditempati oleh pelanggan, dan selanjutnya otomatis pesanan akan dikirimkan ke bagian kitchen dan bar',
       intro2: 'Kitchen',
       cover2:
         'https://1.bp.blogspot.com/-hfcXCdST9Ok/YO6PsKfwFFI/AAAAAAAAPxQ/120jh--K0cs-GIIBK4Asn_FsdfhfKulqACLcBGAsYHQ/s1349/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%252812%2529.png',
-      conintro2: ' ',
+      conintro2:
+        'Kitchen display akan otomatis menampilkan seluruh pesanan yang dikirimkan oleh waiters, dan dengan kemudahan untuk pembagian berdasarkan divisi kitchen misal dapur1,dapur2, bar dan lain nya.',
       intro3: 'Kasir ',
       cover3:
         'https://1.bp.blogspot.com/-mYInFhPeBu8/YO6PuPPMY7I/AAAAAAAAPxg/dD1x3FS1XeEvMVLpsbCGVeNTxADoIeXKwCLcBGAsYHQ/s1349/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%252816%2529.png',
-      conintro3: ' ',
+      conintro3:
+        'Dan semakin mempermudah dan mempercepat kasir dalam handling order dengan cepat, dengan memilih meja pelanggan nantinya otomatis akan dimuncul kan detail dari pesanan dan total bayar, disini kasir bisa dengan cepat untuk mencetak bill hingga bertransaksi dan mencetak struk nota kasir.',
       intro4: 'Admin',
       cover4:
         'https://1.bp.blogspot.com/-dU5eQodQIPA/YO6PwVnU7mI/AAAAAAAAPyA/YZTe3LHceIM-JGaVu7-EnZR_oFATGQXyQCLcBGAsYHQ/s682/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%252824%2529.png',
-      conintro4: ' ',
+      conintro4:
+        'Include dengan admin untuk kemudahan membuat database , melakukan konfigurasi dan setting serta pengaturan lain nya, mendaftarkan dan mendata pelanggan, supplier,membuat menu makanan minuman dan produk , membuat pilihan menu dan porsi , melakukan transaksi resep dengan pencatatan stok inventori , membagi staff bekerja berdasarkan divisi masing masing, dan kebutuhan lain nya untuk administrasi.',
       intro5: 'Accounting ',
       cover5:
         'https://1.bp.blogspot.com/-xIq25dpLWfg/YO6PwCqpvVI/AAAAAAAAPx8/3FfL80RKG10cfUpfcTcN85PlwmkKw_nJwCLcBGAsYHQ/s682/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%252823%2529.png',
-      conintro5: ' ',
+      conintro5:
+        ' Include dengan keuangan neraca dan pencatatan akun akan kas accounting akuntansi dalam bekerja untuk restoran cafe dan rumah makan maupun usaha kuliner lain nya, dilengkapi dengan master data akuntansi untuk membuat data pembukuan dan arus , kemudian laporan laporan detail mengenai accounting juga tersedia untuk kemudahan cek dan audit laporan keuangan restoran rumah makan cafe.',
       intro6: ' Website',
       cover6:
         'https://1.bp.blogspot.com/-pvJ0plk6zTA/YO6Px2V1SmI/AAAAAAAAPyU/ssKtYame_EYFtDvh_AHrUNmyA03M6t9GgCLcBGAsYHQ/s668/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%252829%2529.png',
-      conintro6: ' ',
+      conintro6:
+        'Dan bukan sekedar aplikasi restoran saja tetapi sudah include dengan website yang terintegrasi penuh dalam mendukung kemajuan bisnis kuliner anda , pun dukungan akan android app semakin mendekatkan pada pelanggan untuk update informasi dari website restoran melalui android app bundle ini. paket lengkap all in one sudah termasuk dengan aplikasi restoran online , website restoran dengan nama domain namaresto.com dan cloud hosting siap merubah dan upgrade bisnis mu menjadi lebih powerfull.',
       intro7: 'Online Order',
       cover7:
         'https://1.bp.blogspot.com/-_HRcqqdx_9U/YO6PpUiBGtI/AAAAAAAAPw8/58-ATQqkAfsTgjxw5Q_bgzcJ1s2_6DRZACLcBGAsYHQ/s864/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%25281%2529.png',
-      conintro7: ' ',
+      conintro7:
+        'Kemudahan untuk melakukan online order hadir untuk handling pemesanan secara online langsung via website restoran cafe rumah makan.',
       intro8: 'QrCode',
       cover8:
         'https://1.bp.blogspot.com/-73B-yfK0FUY/YO6PpkG6W4I/AAAAAAAAPxE/NirKfY62Bj4Na1Jmttx23u5ZWXDL_hcWQCLcBGAsYHQ/s1080/aplikasi%2Brumah%2Bmakan%2Brestoran%2Bcafe%2Bkuliner%2Bwebsite%2Bonline%2Bshop%2B%252830%2529.png',
-      conintro8: ' ',
-      intro9: ' All in one',
+      conintro8: 'Payment digital dan Menu digital dengan QR Code tersedia',
+      intro9: ' All in one Resto web app lengkap',
       cover9:
         'https://img.freepik.com/free-vector/interior-small-family-restaurant_88100-295.jpg',
-      conintro9: ' ',
-      pos1: 'Touchscreen V.1',
+      conintro9:
+        'Saat nya kini beralih ke bang resto untuk solusi terbaik untuk kebutuhan mu, dengan konsep aplikasi kasir restoran online include pembukuan lengkap dan dukungan penuh website dan android app maka ini adalah revolusi digital untuk usaha kuliner mu. Dan pilih mesin kasir real point of sale dengan dukungan hardware device terbaik dalam kebutuhan kestabilan dan kecepatan dalam bekerja.',
+      pos1: 'Touchscreen V.2',
       coverpos1:
         'https://1.bp.blogspot.com/-MwAHO7kI1yc/X9gyzcGOsUI/AAAAAAAALrI/VY3uPZTtNEolN05q2jL2ipGhh2mJZ4HoACLcBGAsYHQ/s1400/mesin%2Bkasir%2Btouchscreen%2Blayar%2Bsentuh%2BTERMURAH.jpg',
-      spekpos1: ' ',
-      pos2: 'Touchscreen V.2 ',
+      spekpos1:
+        'All in one touchscreen by ASUS technology + Printer kasir auto cutter 80mm yang mampu memotong kertas otomatis + remote desktop software untuk layanan bantuan + aplikasi integrasi website resto untuk mu.',
+      pos2: 'Touchscreen V.3 ',
       coverpos2:
         'https://1.bp.blogspot.com/-3eRSQUaWhI0/X9gyzZgsb6I/AAAAAAAALrM/NYyNgud3rX8oX4Sa7kdN_0-rvV2984pJACLcBGAsYHQ/s1400/mesin%2Bkasir%2Btouchscreen%2Blayar%2Bsentuh3.jpg',
-      spekpos2: ' ',
-      pos3: 'Touchscreen V.3 ',
+      spekpos2:
+        'All in one touchscreen by ASUS technology + Printer kasir auto cutter 80mm yang mampu memotong kertas otomatis + laci kasir bahan full besi + remote desktop software untuk layanan bantuan + aplikasi integrasi website resto untuk mu.',
+      pos3: 'Touchscreen V.4 ',
       coverpos3:
         'https://1.bp.blogspot.com/-pmvELY2t30A/X9gy0enKUrI/AAAAAAAALrQ/Q15O66wKo9kEu-1kJXxmhVPSJ_cxKt0qwCLcBGAsYHQ/s1400/mesin%2Bkasir%2Btouchscreen%2Blayar%2Bsentuh4.jpg',
-      spekpos3: ' ',
-      pos4: 'Touchscreen V.4 ',
+      spekpos3:
+        'All in one touchscreen by ASUS technology + Printer kasir auto cutter 80mm yang mampu memotong kertas otomatis + laci kasir bahan full besi + manual laser barcode + remote desktop software untuk layanan bantuan + aplikasi integrasi website resto untuk mu.',
+      pos4: 'Touchscreen V.5 ',
       coverpos4:
         'https://1.bp.blogspot.com/-f2Ah3r32hxE/X9gy0ke_aAI/AAAAAAAALrU/2wCynIYp8fEr-Z80k-ce3qL5Fr-r0XzSACLcBGAsYHQ/s1400/mesin%2Bkasir%2Btouchscreen%2Blayar%2Bsentuh5.jpg',
-      spekpos4: ' ',
-      pos5: 'Touchscreen V.5 ',
+      spekpos4:
+        'All in one touchscreen by ASUS technology + Printer kasir auto cutter 80mm yang mampu memotong kertas otomatis + laci kasir bahan full besi + auto multi laser barcode + remote desktop software untuk layanan bantuan + aplikasi integrasi website resto untuk mu.',
+      pos5: 'Touchscreen V.6 ',
       coverpos5:
         'https://1.bp.blogspot.com/-clEilHQCj54/X9gy05Q5-WI/AAAAAAAALrY/u3L1aiQT5hQSm7i4F9-Av2ZhduEF3wGqQCLcBGAsYHQ/s1400/mesin%2Bkasir%2Btouchscreen%2Blayar%2Bsentuh6.jpg',
-      spekpos5: ' ',
+      spekpos5:
+        'All in one touchscreen by ASUS technology + Printer kasir auto cutter 80mm yang mampu memotong kertas otomatis + laci kasir bahan full besi + auto multi laser barcode + mesin pencetak kode barcode + remote desktop software untuk layanan bantuan + aplikasi integrasi website resto untuk mu',
       info: 'Contact Us.',
       coverinfo:
         'https://media.istockphoto.com/vectors/modern-coffeehouse-coffee-shop-interior-design-vector-id1126278617?b=1&k=20&m=1126278617&s=612x612&w=0&h=IOV_ymvsNOIgrzD0i5wuZICE_-4BUQ90Yb5dr6kewqs=',
